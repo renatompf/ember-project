86a896b87f5891779c2c99e51acf541234a6be3e
renatompf <renatompf@hotmail.com>
2025-04-20 21:49:59 +0200

    Add custom HTTP and framework annotation definitions
    Introduce annotations for HTTP methods (Get, Post, Put, Delete, etc.), as well as Service and Controller annotations for framework usage. These annotations simplify API handling and class organization by offering declarative metadata support.

---
+++ src/main/java/io/ember/annotations/controller/Controller.java
@@ -0,0 +1,13 @@
+package io.ember.annotations.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Controller {
+    String value() default ""; // The base path for the controller, default is an empty string
+}
---
+++ src/main/java/io/ember/annotations/service/Service.java
@@ -0,0 +1,11 @@
+package io.ember.annotations.service;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Service {
+}
